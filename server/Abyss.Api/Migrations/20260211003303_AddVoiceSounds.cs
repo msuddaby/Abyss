3f9762675f0c28c7cb89c20824080a236235eccd
Matthew Suddaby <matthewsuddaby@hotmail.com>
2026-02-10 18:10:33 -0700

    call join / leave sounds migration

---
+++ server/Abyss.Api/Migrations/20260211003303_AddVoiceSounds.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Abyss.Api.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddVoiceSounds : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "JoinSoundUrl",
+                table: "UserPreferences",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "LeaveSoundUrl",
+                table: "UserPreferences",
+                type: "text",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JoinSoundUrl",
+                table: "UserPreferences");
+
+            migrationBuilder.DropColumn(
+                name: "LeaveSoundUrl",
+                table: "UserPreferences");
+        }
+    }
+}
